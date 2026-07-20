2758218a147256d966a66912704e439ebf7a1335
Alfredo Diani <alfredodiani@hotmail.com>
2026-07-20 16:22:37 -0300

    heranca - upcasting e downcasting - overide

---
+++ src/Sessao13_Heranca_e_Polimorfismo/a48_sobreposicao_overide_super/application/Program.java
@@ -0,0 +1,38 @@
+package Sessao13_Heranca_e_Polimorfismo.a48_sobreposicao_overide_super.application;
+
+import Sessao13_Heranca_e_Polimorfismo.a48_sobreposicao_overide_super.entities.Account;
+import Sessao13_Heranca_e_Polimorfismo.a48_sobreposicao_overide_super.entities.BusinessAccount;
+import Sessao13_Heranca_e_Polimorfismo.a48_sobreposicao_overide_super.entities.SavingsAccount;
+
+public class Program {
+    public static void main(String[] args) {
+        Account acc = new Account(1001, "Alex", 0.0);
+        BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
+
+        //upcasting
+        Account acc1 = bacc;
+        Account acc2 = new BusinessAccount(1003,"James", 0.0,1200.0);
+        Account acc3 = new SavingsAccount(1004,"Ana", 1000.0, 0.05);
+
+        //Downcasting
+
+        BusinessAccount acc4 = (BusinessAccount)acc2;
+        acc4.loan(100.0);
+
+        //BusinessAccount acc5 = (BusinessAccount)acc3;
+        if (acc3 instanceof BusinessAccount){
+            BusinessAccount acc5 = (BusinessAccount)acc3;
+            acc5.loan(120.0);
+            System.out.println("Loan!");
+        }
+
+        if (acc3 instanceof SavingsAccount){
+            SavingsAccount acc5 = (SavingsAccount)acc3;
+            acc5.updateBalance();
+            System.out.println("Update!");
+        }
+
+
+
+    }
+}
